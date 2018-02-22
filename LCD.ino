89b069e57d5459af48917cd8740879fd2cc3b4c2
Colin de Roos <colinderoos@hotmail.nl>
2018-02-22 18:17:40 +0100

    move LCD en pin to 12 and mosfet to 13
    this way we can use the builtin LED instead of an extra circuit with te mosfet

--- LCD.ino
+++ LCD.ino
@@ -1,6 +1,6 @@
 #include <LiquidCrystal.h>
 
-const int rs = 8, en = 13, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
+const int rs = 8, en = 12, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setupLCD() {
