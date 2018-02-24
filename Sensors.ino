0f3c2fc2a4a04b550356d018a6cce4f4d9c219e4
MaaikeG <maaikegalama@outlook.com>
2018-02-24 13:38:01 +0100

    Use NewPing Lebrary for motion detection

--- Sensors.ino
+++ Sensors.ino
@@ -1,35 +1,38 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <NewPing.h>
 
 #define ONE_WIRE_BUS 14 // Temperature sensor input
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
-unsigned long lastTempReading = 0;
-int tempReadDelay = 30000; // read temp once every 30 seconds
+#define trig  16  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define echo  17  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define maxDistance 100 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+
+NewPing sonar(trig, echo, maxDistance); // NewPing setup of pins and maximum distance.
+
 float temperature;
+unsigned long lastTempReading = 0;
+int tempReadDelaySeconds = 30;  
  
 const int magnet = 19;
-const int trig = 16; // Distance sensor trigger
-const int echo = 17; // Distance sensor echo
-long duration, cm, inches;
+const int motion = 18;
 
 const uint8_t debounceDelay = 50;
 unsigned long lastDebounceTime;
 
-int lastButtonStates;
-int currentButtonStates; // contains current and last states of each button.
+unsigned int lastButtonStates;
+unsigned int currentButtonStates; // contains current and last states of each button.
 // The bit on the position that is also the index of the button contains
 // the last state for that button.
 
 void setupSensors() {
   sensors.begin();
-  pinMode(trig, OUTPUT);
-  pinMode(echo, INPUT);
 }
 
 int getTemperature() {
-  clockWatch(tempReadDelay, &lastTempReading, [](){
+   clockWatch(tempReadDelaySeconds * 1000, &lastTempReading, [](){
      sensors.requestTemperatures(); // Send the command to get temperatures
      temperature = sensors.getTempCByIndex(0);
   });
@@ -38,33 +41,19 @@ int getTemperature() {
 }
 
 int getDistance() {
-  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
-  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
-  digitalWrite(trig, LOW);
-  delayMicroseconds(5);
-  digitalWrite(trig, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trig, LOW);
-
-  // Read the signal from the sensor: a HIGH pulse whose
-  // duration is the time (in microseconds) from the sending
-  // of the ping to the reception of its echo off of an object.
-  duration = pulseIn(echo, HIGH);
-
-  // convert the time into a distance
-  cm = (duration / 2) / 29.1;
-
-  Serial.print(cm);
-  Serial.print("cm");
-  Serial.println();
+   Serial.print(sonar.ping_cm());
 }
 
 int getMagnetState() {
   return debouncedDigitalRead(magnet);
 }
 
+int getMotionState() {
+  return digitalRead(motion);
+}
+
 bool debouncedDigitalRead(int buttonPin) {
-  uint8_t bitPosition = buttonPin / 2;
+  int bitPosition = buttonPin;
   int currentState = (currentButtonStates & (1 << bitPosition)) > 0;
   int lastState = (lastButtonStates & (1 << bitPosition)) > 0;
 
